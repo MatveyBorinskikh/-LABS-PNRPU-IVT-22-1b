20df7c3ec611a75fa702fe7611dcc700ea2b6935
MatveyBorinskikh <117202643+MatveyBorinskikh@users.noreply.github.com>
2022-12-21 15:30:38 +0500

    (╯°益°)╯彡┻━┻

---
+++ Задачи/2-23/2-23.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    float n, a, max;
+    int i = 2;
+    int count = 1;
+
+    std::cout << "Введите n: " << std::endl;
+    std::cin >> n;
+
+    max = sin(n + 1 / n);
+
+    while (i <= n)
+    {
+        a = sin(n + 1 / n);
+        if (a > max)
+        {
+            max = a;
+            count = 1;
+        }
+        else if (a == max)
+        {
+            count++;
+        }
+        i++;
+    }
+
+    std::cout << "Максимальный элемент: " << max << " Его номер: " << count << std::endl;
+    std::cout << "Максимальный элемент: " << max << std::endl;
+    std::cout << "Число элементов с этим значением: " << count << std:: endl;
+}
