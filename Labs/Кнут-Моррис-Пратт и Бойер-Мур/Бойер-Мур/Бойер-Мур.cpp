fc9eca0c92ca575731a10de4335054a87e04eaa3
MatveyBorinskikh <117202643+MatveyBorinskikh@users.noreply.github.com>
2023-03-29 01:14:40 +0500

    (╯°益°)╯彡┻━┻

---
+++ Labs/Кнут-Моррис-Пратт и Бойер-Мур/Бойер-Мур/Бойер-Мур.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int find(string str, string substr)
+{
+    int strSize = str.size();
+    int substrSize = substr.size();
+
+    if (strSize != 0 && substrSize != 0)
+    {
+        int pos;
+        int buff[256];
+        for (int i = 0; i < 256; i++)
+        {
+            buff[i] = substrSize;
+        }
+        for (int i = substrSize - 2; i >= 0; i--)
+        {
+            int subStrPos = int((unsigned char)substr[i]);
+            if (buff[subStrPos] == substrSize)
+            {
+                buff[subStrPos] = substrSize - i - 1;
+            }
+            pos = substrSize - 1;
+            while (pos < strSize)
+            {
+                if (substr[substrSize - 1] != str[pos])
+                {
+                    pos += buff[int((unsigned char)str[pos])];
+                }
+                else
+                {
+                    for (int i = substrSize - 1; i >= 0; i--)
+                    {
+                        if (substr[i] != str[pos - substrSize + i + 1])
+                        {
+                            pos += buff[int((unsigned char)str[pos - substrSize + i + 1])];
+                            break;
+                        }
+                        else if (i == 0)
+                        {
+                            return pos - substrSize + 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    string inputStr, searchStr;
+    cout << "Ââåäèòå ñòðîêó" << endl;
+    cin >> inputStr;
+    cout << "Ââåäèòå îáðàç" << endl;
+    cin >> searchStr;
+    int idx = find(inputStr, searchStr);
+    if (idx == -1)
+    {
+        cout << "Îáðàç â ñòðîêå íå íàøåëñÿ";
+    }
+    else
+    {
+        cout << idx;
+    }
+    return 0;
+}
---
+++ Labs/Кнут-Моррис-Пратт и Бойер-Мур/Кнут-Моррис-Пратт/Кнут-Моррис-Пратт.cpp
@@ -0,0 +1,88 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+void prefix(string substr, int substrSize, int* pref);
+
+void KMPSearch(string substr, string str)
+{
+	int strSize = str.size();
+	int substrSize = substr.size();
+
+	int* pref = new int[substrSize];
+
+	prefix(substr, substrSize, pref);
+
+	int i = 0; // index for str[]
+	int j = 0; // index for substr[]
+
+	while ((strSize - i) >= (substrSize - j))
+	{
+		if (substr[j] == str[i])
+		{
+			j++;
+			i++;
+		}
+
+		if (j == substrSize)
+		{
+			cout << "Ïîäñòðîêà íàøëàñü ïî èíäåêñó:" << i - j << endl;
+			j = pref[j - 1];
+		}
+
+		else if (i < strSize && substr[j] != str[i])
+		{
+			if (j != 0)
+			{
+				j = pref[j - 1];
+			}
+			else
+			{
+				i = i + 1;
+			}
+		}
+	}
+}
+
+void prefix(string substr, int substrSize, int* pref)
+{
+	int len = 0;
+
+	pref[0] = 0;
+
+	int i = 1;
+	while (i < substrSize)
+	{
+		if (substr[i] == substr[len])
+		{
+			len++;
+			pref[i] = len;
+			i++;
+		}
+		else
+		{
+			if (len != 0)
+			{
+				len = pref[len - 1];
+			}
+			else
+			{
+				pref[i] = 0;
+				i++;
+			}
+		}
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	string str;
+	string substr;
+	cout << "Ââåäèòå ñòðîêó: ";
+	cin >> str;
+	cout << "Ââåäèòå ïîäñòðîêó,êîòîðóþ õîòèòå íàéòè: ";
+	cin >> substr;
+	KMPSearch(substr, str);
+	return 0;
+}
