1313a0aea89f6918c96de91dd9f9ef957fd2d795
MatveyBorinskikh <117202643+MatveyBorinskikh@users.noreply.github.com>
2023-03-29 01:54:35 +0500

    (╯°益°)╯彡┻━┻

---
+++ Labs/10 labs/10.cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a[1000], n, k, l, s;
+	cin >> n >> m >> k; //m - заданный номер
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+	cin >> l >> s; //l - index, s - число
+
+	for (int i = m; i < n - k; i++)
+		for (int j = i + k; i < n; i++)
+			a[i] = a[j];
+	n -= k;
+
+	for (int i = 0; i < n; i++)
+		cout << a[i] << ' ';
+	cout << endl;
+
+	for (int i = n - 1; i >= l; i--)
+		a[i + 1] = a[i];
+	n++;
+	a[l] = s;
+
+
+	for (int i = 0; i < n; i++)
+		cout << a[i] << ' ';
+	cout << endl;
+
+	return 0;
+}
---
+++ Labs/9 labs/9.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <string>
+using namespace std;
+int main()
+{
+    int k = 0, j = 0;
+    char f2[255], f3[255], * p;
+    ifstream f("F1.txt");
+    ofstream f1("F2.txt");
+
+    while (f)
+    {
+        f.getline(f2, 255);
+        k++;
+        if (k > 3)
+        {
+            f1 << f2 << "\n";
+        }
+        //cout<<f2<<"\n";
+    }
+    ifstream p1("F1.txt");
+    while (p1)
+    {
+        p1.getline(f3, 255);
+        j++;
+        if (j == k - 1)
+        {
+            cout << f3 << "\n";
+
+            p = strtok(f3, " ");
+            cout << p;
+        }
+    }
+
+    f.close();
+    f1.close();
+    p1.close();
+
+
+    ifstream f4("F2.txt");
+    char ch;
+    int amount = 0;
+    int last_word = 0;
+    while (f4)
+    {
+        f4.get(ch);
+        if (ch == ' ' || ch == '\t' || ch == '\n')
+        {
+            if (amount > 0)
+            {
+                last_word = amount;
+            }
+            amount = 0;
+        }
+        else
+            amount++;
+    }
+
+    if (amount > 0)
+        last_word = amount;
+
+    f4.close();
+
+    cout << endl;
+    cout << "Last word have " << last_word << " symbols" << endl;
+
+    system("pause");
+
+
+    return 0;
+}
